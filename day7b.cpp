750f91f71488371775fe30b79da5d423ada9718b
Mubeen Kodvavi <mubeen.kodvavi@gmail.com>
2021-12-07 14:50:43 +0500

    day7

---
+++ day7b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    stringstream ss(str);
+    vector<int> positions;
+    while (ss.good())
+    {
+        string substr;
+        getline(ss, substr, ',');
+        positions.push_back(stoi(substr));
+    }
+    int minCost = INT_MAX;
+    for (int i = 0; i < 2000; i++)
+    {
+        int cost = 0;
+        for (int j = 0; j < positions.size(); j++)
+        {
+            cost += (abs(i - positions[j]) * (abs(positions[j] - i) + 1)) / 2;
+        }
+        minCost = min(cost, minCost);
+    }
+    cout << minCost;
+}
